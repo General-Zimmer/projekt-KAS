15db83ce6d603600e0c679a5722d7eb167c4e654
Maksi333 <Simon_n_andersen@live.dk>
2023-05-08 10:11:53 +0200

    Jeg har lavet hav og himmel weee

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,7 +1,23 @@
 package gui;
 
+import model.Cumferemsæ;
+
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) {
+
+    }
+
+    public static void initStorage(){
+        //-------------------Hav og himmel--------------
+        LocalDate startDate = LocalDate.of(2023,8,01);
+        LocalDate endDate = LocalDate.of(2023,8,11);
+
+        Cumferemsæ havOgHimmel = new Cumferemsæ("Hav og Himmel", "Odense Uni", startDate, 10, 1500);
+
+        //--------------------Deltagere-------------------
+
 
     }
 }
