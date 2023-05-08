4a9ac27778f56b9ae065aa247b01cd1142480794
Maksi333 <Simon_n_andersen@live.dk>
2023-05-08 11:59:08 +0200

    oprettet Participant

---
+++ src/model/Participant.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Participant {
+    private String name; //First and last name
+    private String phoneNumber;
+    private String email;
+
+    public Participant(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
