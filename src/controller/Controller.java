ead120fc788004e5ef5a975a17dff48a1381d05c
GZimmer <kristianz1000@gmail.com>
2023-05-08 09:44:56 +0200

    fixed CreateCumfomragner

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,6 +8,8 @@ public abstract class Controller {
 
     public static Cumferemsæ createConference(String name, String sted,LocalDate startDate, int periode, float dayPrice) {
         Cumferemsæ cumferemsæ = new Cumferemsæ(name, sted, startDate, periode, dayPrice);
+
+        return cumferemsæ;
     }
 
 }
