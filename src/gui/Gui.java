f6e8a719474eec994577a2ce52bc4ae063936e14
Maksi333 <Simon_n_andersen@live.dk>
2023-05-10 09:47:27 +0200

    noget gui

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -3,21 +3,21 @@ package gui;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
-import java.util.List;
+import model.Hotel;
+import model.Konference;
 
 public class Gui extends Application {
+
+    private final ListView<Hotel> lvwHoteller = new ListView<>();
+    private final ListView<Konference> lvwKonfListe = new ListView<>();
+    private final Button btnOpretKonf = new Button("Opret Konference");
+    private final TextField txfStartDato = new TextField();
+    private final TextField txfPeriode = new TextField();
+    private final TextField txfKonfNavn = new TextField();
+
 
 
     @Override
@@ -25,6 +25,10 @@ public class Gui extends Application {
         stage.setTitle("KAS-iing!");
         GridPane pane = new GridPane();
         this.initContent(pane);
+        pane.setPadding(new Insets(10));
+        pane.setHgap(10);
+        pane.setVgap(10);
+
 
         Scene scene = new Scene(pane);
         stage.setScene(scene);
@@ -34,6 +38,14 @@ public class Gui extends Application {
 
 
     private void initContent(GridPane pane) {
+        Label konferenceLabel = new Label("Konference");
+        pane.add(konferenceLabel,1,0);
+        pane.add(lvwKonfListe,1,1);
+        pane.add(txfStartDato,1,2);
+        pane.add(txfPeriode,1,3);
+        pane.add(txfKonfNavn,1,4);
+        pane.add(btnOpretKonf,1 ,5);
+
 
 
     }
