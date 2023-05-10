db0f1ffcfb65faf00f353901216cb57f36417465
Maksi333 <Simon_n_andersen@live.dk>
2023-05-10 11:06:02 +0200

    noget gui

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -18,12 +18,13 @@ public class Gui extends Application {
     private final TextField txfStartDato = new TextField();
     private final TextField txfPeriode = new TextField();
     private final TextField txfKonfNavn = new TextField();
+    private final TextField txfPrisPrDag = new TextField();
 
 
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("KAS-iing!");
+        stage.setTitle("Konference bookingssystem");
         GridPane pane = new GridPane();
         this.initContent(pane);
         pane.setPadding(new Insets(10));
@@ -51,20 +52,32 @@ public class Gui extends Application {
         Label lblKonfNavn = new Label("Konference");
         pane.add(lblKonfNavn,0,4);
         pane.add(txfKonfNavn,1,4);
-        pane.add(btnOpretKonf,1 ,3);
+        pane.add(btnOpretKonf,1 ,6);
+        Label lblPris = new Label("Pris pr dag");
+        pane.add(lblPris,0,5);
+        pane.add(txfPrisPrDag,1,5);
+        HBox hbox4 = new HBox();
+
+
 
         HBox hbox1 = new HBox();
         hbox1.getChildren().addAll(lblStartDato, txfStartDato);
-        hbox1.setSpacing(5);
+        hbox1.setSpacing(22);
         pane.add(hbox1,0,2);
         HBox hBox2 = new HBox();
         hBox2.getChildren().addAll(lblPeriode, txfPeriode);
-        hBox2.setSpacing(5);
+        hBox2.setSpacing(30);
         pane.add(hBox2,0,3);
         HBox hBox3 = new HBox();
         hBox3.getChildren().addAll(lblKonfNavn, txfKonfNavn);
-        hBox3.setSpacing(5);
+        hBox3.setSpacing(10);
         pane.add(hBox3,0,4);
+        hbox4.getChildren().addAll(lblPris, txfPrisPrDag);
+        pane.add(hbox4,0,5);
+        hbox4.setSpacing(12);
+        HBox hbox5 = new HBox();
+        hbox5.getChildren().addAll(btnOpretKonf);
+        pane.add(hbox5,0,6);
 
 
 
