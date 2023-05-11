f63ce3ba7392b5f722564aa8714cccc75d85c221
Maksi333 <Simon_n_andersen@live.dk>
2023-05-11 09:50:55 +0200

    Zimmer bakcup

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -10,19 +10,27 @@ import javafx.stage.Stage;
 import model.Hotel;
 import model.Konference;
 import org.w3c.dom.events.Event;
+import model.Person;
 
 public class Gui extends Application {
 
     private final ListView<Hotel> lvwHoteller = new ListView<>();
     private final ListView<Konference> lvwKonfListe = new ListView<>();
+    private final ListView<Person> lvwPersoner = new ListView<Person>();
     private final Button btnOpretKonf = new Button("Opret Konference");
     private final TextField txfStartDato = new TextField();
     private final TextField txfPeriode = new TextField();
     private final TextField txfKonfNavn = new TextField();
     private final TextField txfPrisPrDag = new TextField();
     private final Button btnOpretKonferencen = new Button("Opret Konference");
-
+    private final Button btnOpretHotel = new Button("Opret hotel");
+    private final Button btnOpretDeltager = new Button("Opret Deltager");
     private Stage opretKonStage;
+    private Stage opretHotelStage;
+    private TextField txfHotelNavn = new TextField();
+    private TextField txfPlacering = new TextField();
+    private Button btnOpretHotel2 = new Button("Opret Hotel");
+    private Stage opretPersonStage;
 
 
 
@@ -45,16 +53,38 @@ public class Gui extends Application {
 
     private void initContent(GridPane pane) {
 
+
+        pane.add(btnOpretKonf,1 ,5);
+        HBox hbox5 = new HBox();
+        hbox5.getChildren().addAll(btnOpretKonf);
+        pane.add(hbox5,0,5);
+
+        pane.add(btnOpretHotel,1 ,5);
+        HBox hbox7 = new HBox();
+        hbox7.getChildren().addAll(btnOpretHotel);
+        pane.add(hbox7,3,5);
+
+        pane.add(btnOpretDeltager,1 ,5);
+        HBox hbox6 = new HBox();
+        hbox6.getChildren().addAll(btnOpretDeltager);
+        pane.add(hbox6,5,5);
+
+
         Label konferenceLabel = new Label("Konference");
         pane.add(konferenceLabel,0,0);
         pane.add(lvwKonfListe,0,1);
 
-        pane.add(btnOpretKonf,1 ,6);
-        HBox hbox5 = new HBox();
-        hbox5.getChildren().addAll(btnOpretKonf);
-        pane.add(hbox5,0,6);
+        Label hotelLabel = new Label("Hotel");
+        pane.add(hotelLabel,3,0);
+        pane.add(lvwHoteller,3,1);
+
+        Label deltagerLabel = new Label("Deltager");
+        pane.add(deltagerLabel,5,0);
+        pane.add(lvwPersoner,5,1);
 
 
+
+//----------------konference popup-----------
         btnOpretKonf.setOnAction(Event -> createKonferencePopup());
 
         opretKonStage = new Stage();
@@ -62,16 +92,34 @@ public class Gui extends Application {
         GridPane konPane = new GridPane();
         this.initContentForConferenceBooking(konPane);
 
-
-
         Scene scene = new Scene(konPane);
         opretKonStage.setScene(scene);
         opretKonStage.setResizable(false);
         opretKonStage.hide();
+
+
+//------------Hotel popup-----------------------
+
+        opretHotelStage = new Stage();
+        opretHotelStage.setTitle("Hotel oprettelse");
+        GridPane hotelPane = new GridPane();
+        this.initContentCreateHotel(hotelPane);
+
+        Scene hotelScene = new Scene(hotelPane);
+        opretHotelStage.setScene(hotelScene);
+        opretHotelStage.setResizable(false);
+        opretHotelStage.hide();
+
+        btnOpretHotel.setOnAction(Event -> createHotelPopup());
     }
     public void createKonferencePopup() {
 
         opretKonStage.show();
+    }
+
+    public void createHotelPopup(){
+        opretHotelStage.show();
+
     }
     private void initContentForConferenceBooking(GridPane pane){
         Label lblStartDato = new Label("Startdato");
@@ -119,4 +167,36 @@ public class Gui extends Application {
         txfPrisPrDag.clear();
         opretKonStage.hide();
     }
+    public void initContentCreateHotel(GridPane pane){
+        Label hotelNavn = new Label("Hotel Navn");
+        pane.add(hotelNavn,0,2);
+        pane.add(txfHotelNavn,1,2);
+        Label hotelPlacering = new Label("Placering");
+        pane.add(hotelPlacering,0,3);
+        pane.add(txfPlacering,1,3);
+        Label lblKonfNavn = new Label("Konference");
+        pane.add(lblKonfNavn,0,4);
+        pane.add(txfKonfNavn,1,4);
+
+        Label lblPris = new Label("Pris pr dag");
+        pane.add(lblPris,0,5);
+        pane.add(txfPrisPrDag,1,5);
+        HBox hbox4 = new HBox();
+
+        HBox hbox1 = new HBox();
+        hbox1.getChildren().addAll(hotelNavn, txfHotelNavn);
+        hbox1.setSpacing(22);
+        pane.add(hbox1,0,2);
+        HBox hBox2 = new HBox();
+        hBox2.getChildren().addAll(hotelPlacering, txfPlacering);
+        hBox2.setSpacing(30);
+        pane.add(hBox2,0,3);
+
+
+        pane.setPadding(new Insets(10));
+        pane.setHgap(10);
+        pane.setVgap(10);
+        pane.add(btnOpretHotel2,0,6);
+    }
+
 }
