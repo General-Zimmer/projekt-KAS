79a436148acfe28deedba10f3b8cf5d91b2e39a4
GZimmer <kristianz1000@gmail.com>
2023-05-12 11:03:38 +0200

    Updated Hotel

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -7,23 +7,18 @@ public class Hotel {
     private String name;
     private final ArrayList<TilKøb> tilkøb = new ArrayList<>();
 
-
     public Hotel(String name){
         this.name = name;
     }
-
     public void addTilkøb(TilKøb tilKøb) {
         this.tilkøb.add(tilKøb);
     }
-
     public ArrayList<TilKøb> getTilkøb() {
         return new ArrayList<>(tilkøb);
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
